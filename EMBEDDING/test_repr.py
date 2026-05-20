e8e789d4688d65bebc668fa622442178a652f73d
rbricout <bricout.raphael@laposte.net>
2026-05-20 17:55:44 +0200

    cleaning repo

--- EMBEDDING/test_repr.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# Load .npy file
-data = np.load('A1_SINGLE_REPR/10000.fasta.output/A1_closest_aplodontia_ancestor_single_repr_rank_001_alphafold2_ptm_model_1_seed_000.npy')
-
-# Check shape and type
-print(f"Shape: {data.shape}")
-print(f"Type: {data.dtype}")
-print(data)
